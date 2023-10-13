8f01e008e73264f73da13acb0c6ddbbbc4eab339
Soffi Zahir <39215907+Soffi-Zahir@users.noreply.github.com>
2023-10-13 17:55:52 -0400

    fix Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,32 +1,39 @@
-import {iconLink, pageLink} from "../data";
+import { iconLink, pageLink } from "../data";
 
 const Footer = () => {
-    return (
-        <footer className="section footer">
-            <ul className="footer-links">
-                {pageLink.map((e) =>
-                    <li key={e.id}>
-                        <a href={e.href} className="footer-link">{e.text}</a>
-                    </li>
-                )}
-            </ul>
-            <ul className="footer-icons">
-                {iconLink.map((icon) => {
-                    const {id, href, name} = icon
-                    return (
-                        <li key={id}>
-                            <a href={href} target="_blank"  className="footer-icon" rel="noreferrer"><i className={name}></i></a>
-                        </li>
-                    )
-                })}
+  return (
+    <footer className="section footer">
+      <ul className="footer-links">
+        {pageLink.map(({ id, href, text }) => (
+          <li key={id}>
+            <a href={href} className="footer-link">
+              {text}
+            </a>
+          </li>
+        ))}
+      </ul>
+      <ul className="footer-icons">
+        {iconLink.map(({ id, href, name }) => {
+          return (
+            <li key={id}>
+              <a
+                href={href}
+                target="_blank"
+                className="footer-icon"
+                rel="noreferrer"
+              >
+                <i className={name}></i>
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+      <p className="copyright">
+        copyright &copy; Zayzou travel tours company
+        <span id="date">{new Date().getFullYear()}</span> all rights reserved
+      </p>
+    </footer>
+  );
+};
 
-            </ul>
-            <p className="copyright">
-                copyright &copy; Zayzou travel tours company
-                <span id="date"></span> all rights reserved
-            </p>
-        </footer>
-    )
-}
-
-export default Footer
+export default Footer;
